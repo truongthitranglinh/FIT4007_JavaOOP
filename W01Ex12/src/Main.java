c6bfb01542357739afa7f7b586e2741303b34724
unknown <kagurayato03110106@gmail.com>
2024-09-15 15:36:23 +0700

    Bai 12

---
+++ W01Ex12/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(string[] args) {
+        int sum=0;
+        for (int i=0;i<=100;i+=7){
+            sum+=1;
+        }
+        System.out.println("tổng các số là bội của 7 là: " + sum);
+    }
+}
